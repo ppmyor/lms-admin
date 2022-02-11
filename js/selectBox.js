2eac9bb74e686006abb3c731dd5d49171e8940e2
ppmyor <yorocobe@gmail.com>
2022-02-11 12:40:35 +0900

    feat: 이미지 업로드 영역 중간 정렬 및 header position fixed 변경

--- js/selectBox.js
+++ js/selectBox.js
@@ -33,8 +33,15 @@ userImage.addEventListener("click", function () {
 function toggleProjectButton() {
     mobileProjectBarButton.classList.toggle(TOGGLE_BUTTON_CLASSNAME);
     mobileGoToMainButton.classList.toggle(TOGGLE_BUTTON_CLASSNAME);
-    projectNav.classList.toggle(TOGGLE_MOBILE_SELECTOR_CLASSNAME);
-    mainArea.classList.toggle(TOGGLE_MOBILE_SELECTOR_CLASSNAME);
+    // projectNav.classList.toggle(TOGGLE_MOBILE_SELECTOR_CLASSNAME);
+    // mainArea.classList.toggle(TOGGLE_MOBILE_SELECTOR_CLASSNAME);
+    if (mobileProjectBarButton.classList.contains(TOGGLE_BUTTON_CLASSNAME)) {
+        mainArea.style.setProperty("display", "none");
+        projectNav.style.setProperty("display", "block");
+    } else {
+        mainArea.style.setProperty("display", "flex");
+        projectNav.style.setProperty("display", "none");
+    }
 }
 
 // toggle user image info
