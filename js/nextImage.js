34d5ef52c48700ce1718242b2d9b75e65ffe7de5
ppmyor <yorocobe@gmail.com>
2022-03-23 15:29:31 +0900

    feat: 다음 이미지 버튼 기능 구현

---
+++ js/nextImage.js
@@ -0,0 +1,37 @@
+const nextImageBtn = document.querySelector("#next-image-button");
+const currentImageArea = document.querySelector(".current-image-area");
+const currentImage = document.querySelector(".current-image");
+const waitImages = document.querySelectorAll(".wait-image");
+
+ASSETS_URL = "../assets/images/sample_image/";
+let pageNum = 1;
+
+nextImageBtn.addEventListener("click", function (event) {
+    event.preventDefault();
+    if (pageNum > waitImages.length) {
+        return;
+    }
+    document.querySelector(".current-image").remove();
+    pageNum += 1;
+    const newImage = document.createElement("img");
+    newImage.src = `./assets/images/sample_image/${pageNum}.jpg`;
+    newImage.alt = "#";
+    newImage.classList.add("current-image");
+    currentImageArea.append(newImage);
+});
+
+for (const image of waitImages) {
+    image.addEventListener("click", function (event) {
+        if (pageNum > waitImages.length) {
+            return;
+        }
+        const clickedImageSrc = event.target.src;
+        document.querySelector(".current-image").remove();
+        const newImage = document.createElement("img");
+        newImage.src = clickedImageSrc;
+        newImage.alt = "#";
+        newImage.classList.add("current-image");
+        currentImageArea.append(newImage);
+        pageNum = parseInt(clickedImageSrc.slice(-5, -4));
+    });
+}
