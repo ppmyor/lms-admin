e60b88ebc6949d0f67a40e7ea90594274c978184
ppmyor <yorocobe@gmail.com>
2022-03-29 16:39:44 +0900

    chore: mainboard js 파일 셋업

---
+++ js/mainboard.js
@@ -0,0 +1,40 @@
+// Tablets, laptops
+// hint
+const hintButton = document.querySelector("#hint-button");
+
+// full-screen
+const fullScreenButton = document.querySelector("#fullscreen-button");
+
+// content description
+const contentDescription = document.querySelector("#content-description-area");
+
+// page
+const pagePreviousButton = document.querySelector(".page-previous");
+const pageHomeButton = document.querySelector(".page-home");
+const pageNextButton = document.querySelector(".page-next");
+
+// font
+const fontSmallButton = document.querySelector(".font-small");
+const fontMediumButton = document.querySelector(".font-medium");
+const fontLargetButton = document.querySelector(".font-large");
+
+// play-pause
+const puaseButton = document.querySelector(".pause-button");
+
+// play speed
+const speedSlowButton = document.querySelector(".speed-slow");
+const speedMediumButton = document.querySelector(".speed-medium");
+const speedFastButton = document.querySelector(".speed-fast");
+
+// Mobile
+// page
+const mobilePagePreviousButton = document.querySelector(".mobile-page-previous");
+const mobilePageHomeButton = document.querySelector(".mobile-page-home");
+const mobilePageNextButton = document.querySelector(".mobile-page-next");
+
+// play-pause
+const mobilePlayButton = document.querySelector(".mobile-play-button");
+const mobilePauseButton = document.querySelector(".mobile-pause-button");
+
+// hint
+const mobileHintButton = document.querySelector(".mobile-hint-button");
