8432d377af658eedc5ed46dc23e2ae1f5cfc464d
ppmyor <yorocobe@gmail.com>
2022-03-30 15:22:05 +0900

    feat: 폰트 크기 조절, 전체화면 기능 추가

--- js/mainboard.js
+++ js/mainboard.js
@@ -1,3 +1,16 @@
+// Mobile
+// page
+const mobilePagePreviousButton = document.querySelector(".mobile-page-previous");
+const mobilePageHomeButton = document.querySelector(".mobile-page-home");
+const mobilePageNextButton = document.querySelector(".mobile-page-next");
+
+// play-pause
+const mobilePlayButton = document.querySelector(".mobile-play-button");
+const mobilePauseButton = document.querySelector(".mobile-pause-button");
+
+// hint
+const mobileHintButton = document.querySelector(".mobile-hint-button");
+
 // Tablets, laptops
 // hint
 const hintButton = document.querySelector("#hint-button");
@@ -26,15 +39,39 @@ const speedSlowButton = document.querySelector(".speed-slow");
 const speedMediumButton = document.querySelector(".speed-medium");
 const speedFastButton = document.querySelector(".speed-fast");
 
-// Mobile
-// page
-const mobilePagePreviousButton = document.querySelector(".mobile-page-previous");
-const mobilePageHomeButton = document.querySelector(".mobile-page-home");
-const mobilePageNextButton = document.querySelector(".mobile-page-next");
+// Tablets, laptops
+// full screen event
+fullScreenButton.addEventListener("click", function () {
+    if (!document.fullscreenElement) {
+        fullScreen(document.body);
+    } else if (document.exitFullscreen) {
+        exitFullScreen();
+    }
+});
 
-// play-pause
-const mobilePlayButton = document.querySelector(".mobile-play-button");
-const mobilePauseButton = document.querySelector(".mobile-pause-button");
+const fullScreen = (element) => {
+    if (element.requestFullscreen) return element.requestFullscreen();
+    if (element.webkitRequestFullscreen) return element.webkitRequestFullscreen();
+    if (element.mozRequestFullScreen) return element.mozRequestFullScreen();
+    if (element.msRequestFullscreen) return element.msRequestFullscreen();
+};
 
-// hint
-const mobileHintButton = document.querySelector(".mobile-hint-button");
+const exitFullScreen = () => {
+    if (document.exitFullscreen) return document.exitFullscreen();
+    if (document.webkitCancelFullscreen) return document.webkitCancelFullscreen();
+    if (document.mozCancelFullScreen) return document.mozCancelFullScreen();
+    if (document.msExitFullscreen) return document.msExitFullscreen();
+};
+
+// font resize event
+fontSmallButton.addEventListener("click", function () {
+    contentDescription.style.fontSize = "1em";
+});
+
+fontMediumButton.addEventListener("click", function () {
+    contentDescription.style.fontSize = "1.2em";
+});
+
+fontLargetButton.addEventListener("click", function () {
+    contentDescription.style.fontSize = "1.5em";
+});
